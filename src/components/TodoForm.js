a93ea9a4327f83d7eebcb465582dde86d91320da
Muzammil <Mohammad.Muzammil@elektrobit.com>
2023-01-10 11:39:07 +0530

    components

---
+++ src/components/TodoForm.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+function TodoForm() {
+    const [input,setInput] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.onSubmit({
+            
+        })
+    }
+    return (
+        <form className='todo-form' onSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            placeholder='Add a todo'
+            value={input}
+            name="text"
+            className='todo-input'
+            />
+            <button className='todo-button'>Add todo</button>
+        </form>
+    )
+}
+
+export default TodoForm
